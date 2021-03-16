2dcd87e56529ea00f4dc49694e6040e6a64a75d1
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-16 11:52:13 +0100

    flatMap test

--- src/tasks/lists/MoreOnLists.scala
+++ src/tasks/lists/MoreOnLists.scala
@@ -2,16 +2,18 @@ package tasks.lists
 
 import u03.Lists
 
-
 object MoreOnLists {
   import Lists._
 
   def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
-    case (List.Cons(_, tail), n) if n > 0 => drop(tail, n - 1)
-    case (List.Cons(h, tail), _) => List.Cons(h, tail)
+    case (List.Cons(_, t), n) if n > 0 => drop(t, n - 1)
+    case (List.Cons(h, t), _) => List.Cons(h, t)
     case _ => List.Nil()
+  }
 
+  def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
+    case List.Cons(head, tail) => List.append(f(head), flatMap(tail)(f))
+    case _ => List.Nil()
   }
 
 }
-
--- test/tasks/lists/ListsTest.scala
+++ test/tasks/lists/ListsTest.scala
@@ -9,9 +9,15 @@ class ListsTest {
 
   @Test def testDrop(){
     val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+    assertEquals(l, MoreOnLists.drop(l, 0))
     assertEquals(List.Cons(20, List.Cons(30, List.Nil())), MoreOnLists.drop(l, 1))
     assertEquals(List.Cons(30, List.Nil()), MoreOnLists.drop(l, 2))
     assertEquals(List.Nil(), MoreOnLists.drop(l, 5))
   }
 
+  @Test def testFlatMap(){
+    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+    assertEquals(List.Cons(11, List.Cons(21, List.Cons(31, List.Nil()))), MoreOnLists.flatMap(l)(v => List.Cons(v+1, List.Nil())))
+    assertEquals(List.Cons(11, List.Cons(12, List.Cons(21, List.Cons(22, List.Cons(31, List.Cons(32, List.Nil())))))), MoreOnLists.flatMap(l)(v => List.Cons(v+1, List.Cons(v+2, List.Nil()))))
+  }
 }
