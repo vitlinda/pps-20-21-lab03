5e0ab1b823bbcf94c46b123f5f01bd41846aa188
Linda Vitali <linda.vitali2@studio.unibo.it>
2021-03-16 12:47:49 +0100

    imports refactoring

--- src/tasks/lists/MoreOnLists.scala
+++ src/tasks/lists/MoreOnLists.scala
@@ -4,24 +4,26 @@ import u03.Lists
 
 object MoreOnLists {
   import Lists._
+  import Lists.List._
 
   def drop[A](l: List[A], n: Int): List[A] = (l, n) match {
-    case (List.Cons(_, t), n) if n > 0 => drop(t, n - 1)
-    case (List.Cons(h, t), _) => List.Cons(h, t)
-    case _ => List.Nil()
+    case (Cons(_, t), n) if n > 0 => drop(t, n - 1)
+    case (Cons(h, t), _) => Cons(h, t)
+    case _ => Nil()
   }
 
   def flatMap[A, B](l: List[A])(f: A => List[B]): List[B] = l match {
-    case List.Cons(head, tail) => List.append(f(head), flatMap(tail)(f))
-    case _ => List.Nil()
+    case Cons(head, tail) => append(f(head), flatMap(tail)(f))
+    case _ => Nil()
   }
 
-  def mapInTermsOfFlat[A,B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(x => List.Cons(mapper(x), List.Nil()))
+  def mapInTermsOfFlat[A,B](l: List[A])(mapper: A => B): List[B] = flatMap(l)(x => Cons(mapper(x), Nil()))
 
   def filterInTermsOfFlat[A](l1: List[A])(pred: A => Boolean): List[A] = l1 match {
-    case List.Cons(h, t) if (pred(h)) => flatMap(List.Cons(h, filterInTermsOfFlat(t)(pred)))(v => List.Cons(v, List.Nil()))
-    case List.Cons(_, t) => filterInTermsOfFlat(t)(pred)
-    case _ => List.Nil()
+    case Cons(h, t) if (pred(h)) => flatMap(Cons(h, filterInTermsOfFlat(t)(pred)))(v => Cons(v, Nil()))
+    case Cons(_, t) => filterInTermsOfFlat(t)(pred)
+    case _ => Nil()
   }
+
 
 }
--- test/tasks/lists/ListsTest.scala
+++ test/tasks/lists/ListsTest.scala
@@ -3,32 +3,36 @@ package tasks.lists
 import org.junit.jupiter.api.Assertions._
 import org.junit.jupiter.api.Test
 import u03.Lists._
+import u03.Lists.List._
 
 
 class ListsTest {
 
   @Test def testDrop(){
-    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
+    val l = Cons(10, Cons(20, Cons(30, Nil())))
     assertEquals(l, MoreOnLists.drop(l, 0))
-    assertEquals(List.Cons(20, List.Cons(30, List.Nil())), MoreOnLists.drop(l, 1))
-    assertEquals(List.Cons(30, List.Nil()), MoreOnLists.drop(l, 2))
-    assertEquals(List.Nil(), MoreOnLists.drop(l, 5))
+    assertEquals(Cons(20, Cons(30, Nil())), MoreOnLists.drop(l, 1))
+    assertEquals(Cons(30, Nil()), MoreOnLists.drop(l, 2))
+    assertEquals(Nil(), MoreOnLists.drop(l, 5))
   }
 
   @Test def testFlatMap(){
-    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
-    assertEquals(List.Cons(11, List.Cons(21, List.Cons(31, List.Nil()))), MoreOnLists.flatMap(l)(v => List.Cons(v+1, List.Nil())))
-    assertEquals(List.Cons(11, List.Cons(12, List.Cons(21, List.Cons(22, List.Cons(31, List.Cons(32, List.Nil())))))), MoreOnLists.flatMap(l)(v => List.Cons(v+1, List.Cons(v+2, List.Nil()))))
+    val l = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), MoreOnLists.flatMap(l)(v => Cons(v+1, Nil())))
+    assertEquals(Cons(11, Cons(12, Cons(21, Cons(22, Cons(31, Cons(32, Nil())))))), MoreOnLists.flatMap(l)(v => Cons(v+1, Cons(v+2, Nil()))))
   }
 
   @Test def mapInTermsOfFlatMapTest(){
-    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
-    assertEquals(List.Cons(false, List.Cons(true, List.Cons(true, List.Nil()))), MoreOnLists.mapInTermsOfFlat(l)(_ > 10))
-    assertEquals(List.Cons(11, List.Cons(21, List.Cons(31, List.Nil()))), MoreOnLists.mapInTermsOfFlat(l)(_ + 1))
+    val l = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(false, Cons(true, Cons(true, Nil()))), MoreOnLists.mapInTermsOfFlat(l)(_ > 10))
+    assertEquals(Cons(11, Cons(21, Cons(31, Nil()))), MoreOnLists.mapInTermsOfFlat(l)(_ + 1))
   }
 
   @Test def filterInTermsOfFlatMapTest(){
-    val l = List.Cons(10, List.Cons(20, List.Cons(30, List.Nil())))
-    assertEquals(List.Cons(20, List.Cons(30, List.Nil())), MoreOnLists.filterInTermsOfFlat(l)(_ >= 20))
+    val l = Cons(10, Cons(20, Cons(30, Nil())))
+    assertEquals(Cons(20, Cons(30, Nil())), MoreOnLists.filterInTermsOfFlat(l)(_ >= 20))
   }
+
+
+
 }
